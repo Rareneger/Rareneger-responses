25d0c7325c34f679e33dbb81b5c4ab829b6fedb7
Tony ALbert Lima <80111558+TonyALima@users.noreply.github.com>
2021-11-09 14:24:50 -0300

    dividing messaging stage and sending stage

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 from send import *
 from message import *
 
+def confirm():
+    answer = input('Confirma? [S/N]: ')
+    answer = answer.upper()
+    if answer == 'S':
+        return True
+    elif answer == 'N':
+        return False
+    else:
+        print('responda com S para sim ou N para não')
+        print('')
+        return confirm()
+
 if __name__ == '__main__':
     messager = Messager()
-    whatsapp =WhatsappSender()
-    whatsapp.do_login()
     msg = messager.make_message()
+    messages = []
     while msg:
-        if msg:
-            message, email, number = msg
-            if number:
-                whatsapp.send_message(message, number)
-            
-            msg = messager.make_message()
+        messages.append(msg)
+        text, email, number = msg
+        
+        if not number and not email:
+            print('* A pessoa não disponibilizou meios de contato')
+        
+        if not confirm():
+            messager.back_row()
+
+        print('')
+        msg = messager.make_message()
+
     messager.on_exit()
+
+    if messages:
+        whatsapp =WhatsappSender()
+        whatsapp.do_login()
+
+        for msg in messages:
+            text, email, number = msg
+            if number:
+                whatsapp.send_message(text, number)
