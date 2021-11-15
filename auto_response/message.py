c9a8fc639a83a6a49a964dc5b0ab6a5c2350b86f
Tony ALbert Lima <80111558+TonyALima@users.noreply.github.com>
2021-11-15 18:03:39 -0300

    showing the message command
    Signed-off-by: Tony ALbert Lima <80111558+TonyALima@users.noreply.github.com>

--- auto_response/message.py
+++ auto_response/message.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from numpy import isnan
 from infos import Info
 
 
@@ -26,54 +25,15 @@ class Messager():
 
     def make_message(self):
         if self.actual_row < len(self.form_df):
-            name = self.form_df['Nome:'][self.actual_row]
-            contact_way = self.form_df['Por onde deseja receber as informações de confirmação do atendimento?'][self.actual_row]
-            contact_number = None
             
-            if 'Whatsapp' in contact_way:
-                num = self.form_df['Para receber por Whatsapp informe o número de celular:'][self.actual_row]
-                if not isnan(num):
-                    contact_number = '55' + str(int(num))
-            
-            if 'Email' in contact_way:
-                contact_email = self.form_df['Para receber por Email informe o Email:'][self.actual_row]
-            else:
-                contact_email = None
-            alternative_payment = ''
-
-            if 'Presencial' in self.form_df['Modalidade'][self.actual_row]:
-                alternative_payment = 'ou presencialmente após o atendimento'
-
             attendance_kind = self.form_df['Atendimento'][self.actual_row].split('-')[0].strip()
             schedule_possibilities = f'{self.form_df["Início"][self.actual_row]} e {self.form_df["Fim"][self.actual_row]}'
 
-            print(f'Marque uma \033[1;32m{attendance_kind}\033[m, a pessoa está disponível entre \033[1;33m{schedule_possibilities}\033[m nos dias \033[1;33m{self.form_df["Disponibilidade de dias da semana"][self.actual_row]}\033[m')
+            command_message = f'Marque uma {attendance_kind}, a pessoa está disponível entre {schedule_possibilities} nos dias {self.form_df["Disponibilidade de dias da semana"][self.actual_row]}'
 
-            mark_date = input('digite a data: ')
-
-            mark_schedule = input('digite o hórario: ')
-
-            text_message = f'Olá {name}, tudo bem? aqui é o Rareneger\n \nA sua {attendance_kind} está marcada para {mark_date} as {mark_schedule}.\nGratidão por se permitir\n \nVocê pode realizar o pagamento pelo pix {alternative_payment}\n \nEsta é uma mensagem automática de confirmação, segue o código para o pix copia e cola.\n \n'
-
-            pay_message = self.pay_codes[attendance_kind]
-
-            message = (text_message, pay_message)
-            
-            print('')
-            print('A mensagem a ser enviada será: ')
-            print('\033[33m-=-\033[m' * 15)
-            print(text_message)
-            print(pay_message)
-            print('\033[33m-=-\033[m' * 15)
-
-            if not contact_number and not contact_email:
-                print('\033[31m* A pessoa não disponibilizou meios de contato\033[m')
-
-            self._update_row()
-
-            return message, contact_email, contact_number
+            return command_message
         else:
-            return None
+            return ''
 
     def on_exit(self):
         last_timestamp = self.form_df['Timestamp'][self.actual_row - 1]
--- auto_response/response_app.py
+++ auto_response/response_app.py
@@ -1,13 +1,16 @@
 from kivy.app import App
+from message import *
 
 class ResponseApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.url = self.get_url_database()
+        self.messager = Messager()
 
     def responder_button(self):
         self.url = self.root.get_screen('menu').children[0].children[1].children[0].text
         self.set_url_database()
+        self.get_message_command()
         self.root.current = 'respostas'
 
     def set_url_database(self):
@@ -20,6 +23,9 @@ class ResponseApp(App):
                 return file.read()
         except FileNotFoundError:
             return ''
+
+    def get_message_command(self):
+        self.root.get_screen('respostas').children[0].children[2].text = self.messager.make_message()
 
 if __name__ == '__main__':
     app = ResponseApp()
