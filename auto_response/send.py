db0b91bb40a59b9ee7a6dfb5613688145c8e22c0
Tony ALbert Lima <80111558+TonyALima@users.noreply.github.com>
2021-11-17 18:56:18 -0300

    cleaning code

--- auto_response/send.py
+++ auto_response/send.py
@@ -1,46 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from selenium import webdriver
-from selenium.common import exceptions
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from time import sleep
 from urllib import parse
 import smtplib
 import email.message
 
-class WhatsappSender():
-    def __init__(self):
-        self.options = Options()
-        self.options.add_argument("user-data-dir=chrome-user-data")
-
-    def send_message(self, message, number, progress, rate=1):
-        url_message = parse.quote(message)
-        url = f'https://web.whatsapp.com/send?phone={number}&text={url_message}'
-        try:
-            with webdriver.Chrome(chrome_options=self.options) as driver:
-                driver.get(url)
-                driver.minimize_window()
-                while len(driver.find_elements(by='id', value='side')) < 1:
-                    sleep(1)
-                progress.update(2 / rate)
-                sleep(5)
-                progress.update(4 / rate)
-                driver.find_element(by='xpath', value='/html/body/div[1]/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]').send_keys(Keys.ENTER)
-                sleep(5)
-                progress.update(4 / rate)
-        except exceptions.WebDriverException:
-            print(f'algo deu errado enviando a mensagem para {number}')
-            print('')
-            print(message)
-            input('Precione Enter para continuar')
-
-    def do_login(self):
-        with webdriver.Chrome(chrome_options=self.options) as driver:
-            driver.get('https://web.whatsapp.com')
-
-            while len(driver.find_elements(by='id', value='side')) < 1:
-                sleep(1)
 
 class EmailSender():
     def __init__(self, infos):
@@ -69,3 +33,4 @@ class EmailSender():
         for line in lines:
             html_message = html_message + '<p>' + line + '</p> \n'
         return html_message
+        
